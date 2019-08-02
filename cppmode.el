a95c67e04fef895019e3525cc165b8d87c6e8970
Juan Tellez <juan@Juans-MacBook-Pro-2.local>
2019-08-02 15:06:17 -0500

    adding cppmode take 2

--- cppmode.el
+++ cppmode.el
@@ -3,3 +3,31 @@
 ;; All of this is borrowed from others, particular credit to:
 ;; byuksel
 ;;
+;; package installs
+
+;; start auto-complete with emacs
+(package-install 'auto-complete)
+(require 'auto-complete)
+
+;; do default config for auto-complete
+(package-install 'auto-complete-c-headers)
+(require 'auto-complete-c-headers)
+
+(require 'auto-complete-config)
+(ac-config-default)
+
+;; start yasnippet with emacs
+(package-install 'yasnippet-snippets)
+(require 'yasnippet-snippets)
+(yas-global-mode 1)
+
+; let's define a function which initializes auto-complete-c-headers and gets called for c/c++ hooks
+(defun my:ac-c-header-init ()
+  (require 'auto-complete-c-headers)
+  (add-to-list 'ac-sources 'ac-source-c-headers)
+  (add-to-list 'achead:include-directories '"/Applications/Xcode.app/Contents/Developer/usr/lib/llvm-gcc/4.2.1/include")
+  (add-to-list 'achead:include-directories '"/usr/local/Cellar/wxmac/3.0.4_1/include/")
+)
+; Now let's call this function from c/c++ hooks
+(add-hook 'c++-mode-hook 'my:ac-c-header-init)
+(add-hook 'c-mode-hook 'my:ac-c-header-init)
--- init.el
+++ init.el
@@ -23,6 +23,24 @@
 ;; Shell mode
 (load-file "~/.emacs.d/shell-mode.el")
 
+;; cppmode
+(load-file "~/.emacs.d/cppmode.el")
+
 ;; Server
 (server-start)
 
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yasnippet-snippets ac-c-headers auto-complete yasnippet auto-complete-c-headers))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
